72f3ae062c8a839f9cb334f1ad88457bc69b0a9c
Mike Kutnik <gimp2312@gmail.com>
2026-07-23 12:45:11 +0200

    fix(zsh): move homebrew path definitions before system libs

--- dotfiles/zsh/.zsh/exports.zsh
+++ dotfiles/zsh/.zsh/exports.zsh
@@ -1,4 +1,4 @@
-export PATH=$HOME/bin:$HOME/.local/bin:/usr/local/bin:/usr/local/bin/env-setup:$PATH
+export PATH="$HOME/bin:$HOME/.local/bin:/usr/local/bin:/usr/local/bin/env-setup:/opt/homebrew/bin:$PATH"
 export LANG=en_US.UTF-8
 
 export BUN_INSTALL="$HOME/.bun"
